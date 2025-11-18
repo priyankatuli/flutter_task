6d99ca767c8b25f97507774562f2436c1cd538fb
priyankatuli <priyankaaatuli123@gmail.com>
2025-11-18 16:13:12 +0600

    design of 2nd ui

---
+++ lib/core/widgets/custom_floating_action_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomFloatingActionButton extends StatelessWidget{
+  final VoidCallback onPressed;
+  final String ? imagePath;
+  final double size;
+  final Color? iconColor;
+
+  const CustomFloatingActionButton({super.key,
+    required this.onPressed,
+    required this.imagePath,
+    this.size = 70,
+    this.iconColor = Colors.white,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+     return FloatingActionButton(onPressed: onPressed,
+       backgroundColor: Colors.transparent,
+       elevation: 0,
+       child: Container(
+         width: size,
+         height: size,
+         decoration: BoxDecoration(
+           gradient: LinearGradient(colors: [
+             Colors.green.shade200,
+             Colors.green.shade700
+           ],
+               begin: Alignment.topLeft,
+               end: Alignment.bottomRight
+           ),
+           shape: BoxShape.circle,
+         ),
+         child: Image.asset(imagePath!,color: iconColor,)
+       ),
+     );
+  }
+
+}
